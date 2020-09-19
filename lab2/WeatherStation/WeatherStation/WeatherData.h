972b4033a7d684e5b73a338f87265d6e9e73559c
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-09-19 21:41:13 +0300

    добавил lab2 с 1-3 заданиями

---
+++ lab2/WeatherStation/WeatherStation/WeatherData.h
@@ -0,0 +1,131 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <string>
+#include "Observer.h"
+
+struct SWeatherInfo
+{
+	double temperature = 0;
+	double humidity = 0;
+	double pressure = 0;
+};
+
+class CDisplay : public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+		Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+		остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		std::cout << "Current Temp " << data.temperature << std::endl;
+		std::cout << "Current Hum " << data.humidity << std::endl;
+		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "----------------" << std::endl;
+	}
+};
+
+class Statistics
+{
+public:
+	void Update(double value)
+	{
+		if (m_min > value)
+		{
+			m_min = value;
+		}
+		if (m_max < value)
+		{
+			m_max = value;
+		}
+		m_acc += value;
+		++m_countAcc;
+	}
+
+	std::string ToString()
+	{
+		return "Max: " + std::to_string(m_max) + "; " + 
+			"Min: " + std::to_string(m_min) + "; " + 
+			"Average: " + std::to_string(m_acc / m_countAcc) + ".";
+	}
+
+private:
+	double m_min = std::numeric_limits<double>::infinity();
+	double m_max = -std::numeric_limits<double>::infinity();
+	double m_acc = 0;
+	unsigned m_countAcc = 0;
+};
+
+class CStatsDisplay : public IObserver<SWeatherInfo>
+{
+private:
+	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
+	Классу CObservable он будет доступен все равно, т.к. в интерфейсе IObserver он
+	остается публичным
+	*/
+	void Update(SWeatherInfo const& data) override
+	{
+		m_temperatureStatistics.Update(data.temperature);
+		m_humidityStatistics.Update(data.humidity);
+		m_pressureStatistics.Update(data.pressure);
+
+		std::cout << "Temperature: " << m_temperatureStatistics.ToString() << std::endl;
+		std::cout << "Humidity" << m_humidityStatistics.ToString()  << std::endl;
+		std::cout << "Pressure" << m_pressureStatistics.ToString() << std::endl;
+	} 
+
+	Statistics m_temperatureStatistics;
+	Statistics m_humidityStatistics;
+	Statistics m_pressureStatistics;
+};
+
+class CWeatherData : public CObservable<SWeatherInfo>
+{
+public:
+	// Температура в градусах Цельсия
+	double GetTemperature()const
+	{
+		return m_temperature;
+	}
+	// Относительная влажность (0...100)
+	double GetHumidity()const
+	{
+		return m_humidity;
+	}
+	// Атмосферное давление (в мм.рт.ст)
+	double GetPressure()const
+	{
+		return m_pressure;
+	}
+
+	void MeasurementsChanged()
+	{
+		NotifyObservers();
+	}
+
+	void SetMeasurements(double temp, double humidity, double pressure)
+	{
+		m_humidity = humidity;
+		m_temperature = temp;
+		m_pressure = pressure;
+
+		MeasurementsChanged();
+		}
+protected:
+	SWeatherInfo GetChangedData()const override
+	{
+		SWeatherInfo info;
+		info.temperature = GetTemperature();
+		info.humidity = GetHumidity();
+		info.pressure = GetPressure();
+		return info;
+	}
+private:
+	double m_temperature = 0.0;
+	double m_humidity = 0.0;
+	double m_pressure = 760.0;
+};
---
+++ lab2/WeatherStationTest/ObserversTest.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "../WeatherStation/WeatherStation/Observer.h"
+#include "../WeatherStation/WeatherStation/WeatherData.h"
+
+typedef IObservable<SWeatherInfo> ObserverType;
+
+class CRemoveDisplay : public IObserver<SWeatherInfo>
+{
+public :
+	CRemoveDisplay(ObserverType& observer)
+	{
+		m_observer = &observer;
+	}
+private:
+	void Update(SWeatherInfo const& data) override
+	{
+		m_observer->RemoveObserver(*this);
+	}
+
+	ObserverType* m_observer;
+};
+
+class ObserverWithStringStream : public IObserver<SWeatherInfo>
+{
+public: 
+	ObserverWithStringStream(std::ostream& outStream, std::string const& data)
+		:m_outStream(outStream),
+		m_data(data)
+	{
+	}
+
+private:
+	void Update(SWeatherInfo const& data) override
+	{
+		m_outStream << m_data;
+	}
+
+	std::ostream& m_outStream;
+	std::string m_data;
+};
